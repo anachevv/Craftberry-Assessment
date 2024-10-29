35142c4b3f7e85c7d88f062b5a46bd71c5256a92
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2024-10-29 17:39:57 +0200

    so far

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,7 +3,7 @@ export default function Quiz() {
     <>
         <div className="container">
             <div className="header">
-                <h1>Build a self care routine suitable for you</h1>
+                <h1 className="bree-serif-regular">Build a self care routine suitable for you</h1>
             </div>
             <div className="paragraph">
                 <p>Take out test to get a personalised self care routine based on your needs.</p>
