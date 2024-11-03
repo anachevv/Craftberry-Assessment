d154c05178d63309305daa10338101ccfc2f61ef
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2024-11-03 23:12:19 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Quiz from './components/Quiz';
 import Question from './components/Question';
 import Results from './components/Results';
+import Products from './components/Results';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Quiz />} />
       <Route path="/question" element={<Question />} />
       <Route path="/results" element={<Results />} />
+      <Route path="/products" element={<Products />} />
       {/* Add more routes as needed */}
     </Routes>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 
-export default function Results() {
+export default function Products() {
     const navigate = useNavigate();
     const [recommendations, setRecommendations] = useState([]);
     const [error, setError] = useState(null);
@@ -22,10 +22,6 @@ export default function Results() {
 
     const redirectToQuestion = () => {
         navigate('/question');
-    };
-
-    const redirectToProducts = () => {
-        navigate('/products');
     };
 
     return (
@@ -49,36 +45,9 @@ export default function Results() {
             </div>
 
             <div className="recommendation-container">
-                <div className="recommendation">
-                    <div className="recommendation-header">
-                        <h3 className="bree-serif-regular">Daily routine</h3>
-                    </div>
-
-                    <div className="recommendation-paragraph">
-                        <p className="poppins-regular">Perfect for if you're looking for soft, nourished skin, our moisturizing body washes are made with skin-natural nutrients that work with your skin to replenish moisture. With a light formula, the bubbly lather leaves your skin feeling cleansed and cared for. And by choosing relaxing fragrances you can add a moment of calm to the end of your day.</p>
-                    </div>
-                </div>
-
                 <div className="recommendations">
+                    <h2>Products</h2>
                     {/* <div className="recommendations-list"> */}
-                    {recommendations.slice(1, 3).map((product) => (
-                        <div key={product.id} className="product-card">
-                            <img src={product.images[0]?.src} alt={product.title} className="product-image" />
-                            <h3 className="bree-serif-regular">{product.title}</h3>
-                            <p className="poppins-regular">{`$${product.variants[0].price}`}</p>
-                        </div>
-                    ))}
-                    {/* </div> */}
-                </div>
-                
-                <div className="more">
-                    <img onClick={redirectToProducts} src="/next.png" alt="next" />
-                </div>
-
-            </div>
-            {/* <div className="recommendations">
-                <h2>Recommended Products</h2>
-                <div className="recommendations-list">
                     {recommendations.map((product) => (
                         <div key={product.id} className="product-card">
                             <img src={product.images[0]?.src} alt={product.title} className="product-image" />
@@ -86,8 +55,15 @@ export default function Results() {
                             <p className="product-price">{`$${product.variants[0].price}`}</p>
                         </div>
                     ))}
+                    {/* </div> */}
                 </div>
-            </div> */}
+                
+                <div className="more">
+                    <img src="/next.png" alt="next" />
+                </div>
+
+            </div>
+            
 
             {/* {error && <p className="error-message">Error: {error}</p>} */}
         </>
